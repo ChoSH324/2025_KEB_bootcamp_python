600134e1b9199c4e49e35a8e7635f4fa2c79c170
choSH324 <12201304@inha.edu>
2025-02-12 11:15:40 +0900

    v 1.7

--- day03.py
+++ day03.py
@@ -26,7 +26,33 @@ def my_pow(b, e) -> float:
         result = result * math.exp(f * math.log(b))
 
     return result
+def my_pow_pro(b, e) -> float:
+    """
+    A user-defined function that receives a base and exponent and returns the power result in the form of a real number
+    :param b: base number
+    :param e: exponent
+    :return: the power result in the form of a real number
+    """
+    result = 1
+    i=int(e)
 
+    if e<0:
+        f = e - i
+        for _ in range(i):  # for k in range(e):
+            result = result * b
+        if f > 0:
+            result = result * math.exp(f * math.log(b))
+    elif e<0:
+        f = -(e - i)
+        for _ in range(-i):  # for k in range(e):
+            result = result * b
+        if f > 0:
+            result = result * math.exp(f * math.log(b))
+        result = 1 / result
+    else:
+        result = 1
+
+    return result
 
 print(my_pow(10, -2))
 print(my_pow(2, 9))
